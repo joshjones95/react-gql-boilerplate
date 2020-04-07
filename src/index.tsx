bf43ce1eed6176698156d689f1da36283d200118
Josh <josh_jones22@live.co.uk>
2020-04-07 19:47:54 +0100

    Add GQL capability

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import { apolloClient } from './plugins/apolloPlugin'
+
 import { Home } from './pages/home'
 
 ReactDOM.render(
-  <Router> 
-    <Route path="/" component={Home} />
-  </Router>
+  <ApolloProvider client={apolloClient}>
+    <Router> 
+      <Route path="/" component={Home} />
+    </Router>
+  </ApolloProvider>
   , 
   document.getElementById('root')
 );
